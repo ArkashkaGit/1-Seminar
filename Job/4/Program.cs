538b0685e79c272ce8b33ec77b2e317bdd5c870f
Arkashka <sevenkapsul@mail.ru>
2022-07-11 18:28:52 +0300

    4 задача выполнена

--- Job/4/Program.cs
+++ Job/4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("Введите любое число больше 1... \n");
+int N = int.Parse(Console.ReadLine());
+int count = 0;
+
+while ( N<=1 )
+{
+    if (N <=1)
+    {
+    Console.WriteLine("это число не подходит, введите другое число");
+    N = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("\Ниже представлен список чётных чисел \nот 1 до указанного вами числа!!!\n");
+
+while (count < N)
+{
+    
+    Console.Write(count + ", ");
+    if (count < N)
+    {
+        count += 2;
+    }
+}

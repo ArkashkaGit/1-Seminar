238aa0efbb8bd1b132686ed314649948298f5c5e
Arkashka <sevenkapsul@mail.ru>
2022-07-11 16:51:04 +0300

    2 задача выполнена

--- Job/2/Program.cs
+++ Job/2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter first number = ");
+int first = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number = ");
+int second = int.Parse(Console.ReadLine());
+
+Console.Write("Enter three number = ");
+int three = int.Parse(Console.ReadLine());
+
+if (first > second)
+{
+    if (first > three)
+    {
+        Console.Write("max number first");
+        Console.Write(" " + first);
+    }
+}
+
+if (second > first)
+{
+    if (second > three)
+    {
+        Console.Write("max number second");
+        Console.Write(" " + second);
+    }
+}    
+
+if (three > first)
+{
+    if (three > second)
+    {
+        Console.Write("max number three");
+        Console.Write(" " + three);
+    }
+}
